1018ee6ef90c878c381d81845a6a69899f91158a
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2024-09-05 22:37:19 +0700

    add

--- Pertemuan 1/3. Luas Segitiga/luas_segitiga.py
+++
@@ -1,6 +0,0 @@
-def luas_segitiga(alas, tinggi):
-    luas = 0.5 * alas * tinggi
-    return luas
-
-hasil = luas_segitiga(25,30)
-print(hasil)
--- Pertemuan 1/6. Volume Kerucut/volume_kerucut.py
+++
@@ -1,7 +0,0 @@
-def volume_kerucut(diameter, tinggi):
-    diameter_to_jarijari = diameter / 2
-
-    volume = 1/3 * 3.14 * diameter_to_jarijari**2 * tinggi
-    return volume
-hasil = volume_kerucut(5,3)
-print(hasil)
